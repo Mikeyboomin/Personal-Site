837d573d0a53be82a36a025330b10c3aa88aff9c
Mikeyboomin <100133629+Mikeyboomin@users.noreply.github.com>
2022-08-28 03:21:47 +0100

    Version 1

--- script.js
+++ script.js
@@ -1,53 +1,33 @@
 const theme = document.getElementById('theme-btn') 
-const icon = document.querySelector('theme-image')
+const icon = document.querySelector('.theme-image')
 const btnText = document.querySelector('.button-text')
 
 theme.onclick = function(){
-    document.body.classList.toggle('light-theme')
+    document.body.classList.toggle('light-theme');
 
+    var theme;
 
     if(document.body.classList.contains('light-theme')){
         btnText.innerText = 'Light';
         icon.src = 'images/sunny.png';
+        theme = 'LIGHT';
     }else{
         btnText.innerText = 'Dark';
-        icon.src = 'images/moon.png'
+        icon.src = 'images/moon.png';
+        theme = 'Dark';
     }
+
+    //saving to local storage
+    localStorage.setItem("PageTheme", JSON.stringify(theme));
+    //converting to JSON
 }
-    
 
+let GetTheme = JSON.parse(localStorage.getItem('PageTheme'));
+    console.log(GetTheme);
 
-
-// theme.onclick = function(){
-//     document.body.classList.toggle('light-theme')
-    
-    
-//     if(document.body.classList.contains('light-theme')){
-//         theme.innerText = 'Light';
-//         // icon.src = 'images/sunny.png';
-//     } else {
-//         theme.innerText = 'Dark';
-//         // icon.src = 'images/moon.png';
-//     }
-    
-// }
-
-// function changeIimage(){
-//     document.getElementById('theme-icon').src="images/sunny.png"
-// }
-
-
-
-
-// function imageChange(){
-//     let mySrc = icon.getAttribute('src')
-// if(mySrc === 'images/moon.png'){
-//     icon.setAttribute('src', 'images/sunny.png')
-// } else{
-//     icon.setAttribute('src', 'images/moon.png')
-// }
-// }
-// imageChange();
+    if(GetTheme === 'LIGHT'){
+        document.body.classList = 'light-theme';
+    }
 
 
 
